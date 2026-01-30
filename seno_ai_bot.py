89d4de1e830545582bbf857c6c783fdabe3ec1a6
idvipseno-cell <idvipseno@gmail.com>
2026-01-30 17:10:40 +0300

    Update seno_ai_bot.py

--- seno_ai_bot.py
+++ seno_ai_bot.py
@@ -84,7 +84,7 @@ class MultiAI:
                         {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
                     ]
                     gemini_model = genai.GenerativeModel(
-                        model_name='gemini-pro',
+                        model_name='gemini-1.5-flash',
                         generation_config=generation_config,
                         safety_settings=safety_settings
                     )
